cba9a7909656ce2d78d47eaadfeedce341c314bf
drennout <serega10lad@gmail.com>
2022-04-11 18:46:20 +0300

    add struct of front app and create nav logic

---
+++ frontend/src/components/account/Profile.js
@@ -0,0 +1,14 @@
+export default function Profile(props) {
+  return (
+    <div className="container">
+      <figure>
+        <blockquote className="blockquote">
+          <p className="mb-0"></p>
+        </blockquote>
+        <figcaption className="blockquote-footer">
+          С уважением, <cite title="Source Title">гавна поеш</cite>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
